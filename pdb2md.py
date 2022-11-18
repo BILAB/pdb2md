f7165b82268eb2cd8a4e5bd08ce20c25b4109f25
k6atru <k6atru@gmail.com>
2022-11-18 15:53:42 +0900

    solve relax and pack problem and make qscript funk

--- pdb2md.py
+++ pdb2md.py
@@ -5,11 +5,18 @@ import requests
 import Bio.PDB as PDB
 import pymol2
 from pyrosetta import *
+from rosetta.core.pack.task import TaskFactory
+from rosetta.core.pack.task import operation
+from rosetta.protocols import minimization_packing as pack_min
+from rosetta.protocols.relax import FastRelax
 import metapredict as meta
 import shutil
 import subprocess
 
-config = configparser.ConfigParser(allow_no_value=True, strict=False)
+config = configparser.ConfigParser(allow_no_value=True,
+                                   strict=False,
+                                   delimiters="=")
+config.optionxform = str
 config.read("config.ini")
 
 def make_ID_dirs(ID_list: list,
@@ -99,7 +106,6 @@ def remove_disordered_residues(pdb_path: str,
     metapredict_domains = meta.predict_disorder_domains(seq).disordered_domain_boundaries
 
     remove_cmd = ""
-    # metapredict_domainsに要素がある場合
     if len(metapredict_domains) != 0:
         for metapredict_domain in metapredict_domains:
             remove_cmd += f"resi {metapredict_domain[0]}:{metapredict_domain[1]} or "
@@ -116,8 +122,8 @@ def remove_disordered_residues(pdb_path: str,
         os.rename("temp.pdb", output_pdb_name)
 
 def preparemd_settings_to_list(config: configparser.ConfigParser,
-                               pdb_path:str,
-                               distdir:str) -> list:
+                               pdb_path: str,
+                               distdir: str) -> list:
     preparemd_config = config.items("PREPAREMD_SETTINGS")
     mol2_name = config["SETTINGS"]["insert_substrate_name"]
     mol2_path = config["PATH"]["parameter_file_path"]
@@ -141,12 +147,72 @@ def preparemd_settings_to_list(config: configparser.ConfigParser,
                 "./", os.path.abspath(".") + os.sep)
     return preparemd_cmd
 
+def rosetta_packing_residues(pdb_path: str,
+                             output_pdb_dir: str,
+                             output_pdb_name: str):
+    init_options = ""
+    for k, v in config.items("ROSETTA_SETTINGS"):
+        init_options += f"-{k} {v} "
+    # init_options = init_options[:-1]
+    init(init_options)
+
+    pose = pose_from_pdb(pdb_path)
+
+    tf = TaskFactory()
+    tf.push_back(operation.InitializeFromCommandline())
+    tf.push_back(operation.RestrictToRepacking())
+
+    packer = pack_min.PackRotamersMover()
+    packer.task_factory(tf)
+
+    if not os.getenv("DEBUG"):
+        packer.apply(pose)
+
+    fr = FastRelax()
+    scorefxn = get_score_function()
+    fr.set_scorefxn(scorefxn)
+    fr.max_iter(10)
+
+    if not os.getenv("DEBUG"):
+        fr.apply(pose)
+
+    pose.dump_pdb(f"{output_pdb_dir}/{output_pdb_name}")
+
+    pymol2_session = pymol2.PyMOL()
+    pymol2_session.start()
+    pymol2_session.cmd.load(f"{output_pdb_dir}/{output_pdb_name}", "packed")
+    pymol2_session.cmd.select("H", "hydro")
+    pymol2_session.cmd.save(F"{output_pdb_dir}/{output_pdb_name}", "packed and not H")
+    pymol2_session.stop()
+
+def make_qscript(par_dir: str,
+                 ID_dirs: dict):
+    dir_list_for_sh_script = ""
+    for ID, dir in ID_dirs.items():
+        dir_list_for_sh_script += f"\t{os.path.basename(dir)}\n"
+
+    with open(f"{par_dir}/qsub.sh", "w") as f:
+        f.write(f"""#!/bin/zsh
+DIR=(\n{dir_list_for_sh_script})
+for i in $DIR
+do
+    echo $1
+    cd $i/amber
+    / usr/local/bin/qsub ./totalrun.sh - N $i
+    cd ../.. /
+done""")
+    f.close()
 
 #%%
 ID_list = [key.upper() for key in config["ID"]]
 ID_dirs = make_ID_dirs(ID_list=ID_list,
                        dir_name=config["SETTINGS"]["workbench_dir_name"])
+
 ID_pdb_paths = {}
+ID_pdb_paths_disordered_removed = {}
+ID_pdb_paths_res_inserted = {}
+ID_pdb_paths_packed = {}
+ID_pdb_paths_res_sub_inserted = {}
 
 for ID, dir in ID_dirs.items():
     if len(ID) == 4:
@@ -170,8 +236,6 @@ for ID, dir in ID_dirs.items():
             f.write(data)
             ID_pdb_paths[ID] = os.path.abspath(f.name)
 
-ID_pdb_paths_disordered_removed = {}
-
 for ID, pdb_path in ID_pdb_paths.items():
     if len(ID) != 4:
         print(f"Removing disordered residues from {ID}...")
@@ -181,48 +245,35 @@ for ID, pdb_path in ID_pdb_paths.items():
     else:
         ID_pdb_paths_disordered_removed[ID] = pdb_path
 
-ID_pdb_paths_mg_inserted = {}
-
 for ID, pdb_path in ID_pdb_paths_disordered_removed.items():
     print(f"Inserting MG into {ID} from templete...")
     insert_templete_residue(residue_name="MG",
-                            output_pdb_name=f"{ID_dirs[ID]}/{ID}_mg_inserted.pdb",
+                            output_pdb_name=f"{ID_dirs[ID]}/{ID}_res_inserted.pdb",
                             pdb_path=pdb_path)
-    ID_pdb_paths_mg_inserted[ID] = f"{ID_dirs[ID]}/{ID}_mg_inserted.pdb"
+    ID_pdb_paths_res_inserted[ID] = f"{ID_dirs[ID]}/{ID}_res_inserted.pdb"
 
-ID_pdb_paths_mg_and_fpp_inserted = {}
+for ID, pdb_path in ID_pdb_paths_res_inserted.items():
+    print(f"Packing {ID} for optimizing sidechains and hetero metals...")
+    rosetta_packing_residues(pdb_path=pdb_path,
+                             output_pdb_dir=ID_dirs[ID],
+                             output_pdb_name=f"{ID}_packed.pdb")
+    ID_pdb_paths_packed[ID] = f"{ID_dirs[ID]}/{ID}_packed.pdb"
 
-for ID, pdb_path in ID_pdb_paths_mg_inserted.items():
+for ID, pdb_path in ID_pdb_paths_packed.items():
     print(f"Inserting FPP into {ID} from templete...")
     insert_templete_residue(residue_name="FPP",
-                            output_pdb_name=f"{ID_dirs[ID]}/{ID}_mg_and_fpp_inserted.pdb",
+                            output_pdb_name=f"{ID_dirs[ID]}/{ID}_res_sub_inserted.pdb",
                             pdb_path=pdb_path)
-    ID_pdb_paths_mg_and_fpp_inserted[ID] = f"{ID_dirs[ID]}/{ID}_mg_and_fpp_inserted.pdb"
+    ID_pdb_paths_res_sub_inserted[ID] = f"{ID_dirs[ID]}/{ID}_res_sub_inserted.pdb"
 
-for ID, pdb_path in ID_pdb_paths_mg_and_fpp_inserted.items():
+for ID, pdb_path in ID_pdb_paths_res_sub_inserted.items():
     print(f"Excuting preparemd.py for {ID}...")
     preparemd_cmd = preparemd_settings_to_list(config=config,
                                                pdb_path=pdb_path,
                                                distdir=ID_dirs[ID])
     subprocess.run(preparemd_cmd)
 
-#%%
-par_dir = config["PATH"]["distination_path"] +"/"+config["SETTINGS"]["workbench_dir_name"]
-
-dir_list = ""
-for ID, dir in ID_dirs.items():
-    dir_list += f"\t{os.path.basename(dir)}\n"
-
-with open(f"{par_dir}/qsub.sh", "w") as f:
-    f.write(f"""#!/bin/zsh
-DIR=(\n{dir_list})
-for i in $DIR
-do
-    echo $1
-    cd $i/amber
-    /usr/local/bin/qsub ./totalrun.sh -N $i
-    cd ../../
-done""")
-    f.close()
-
-# %%
+make_qscript(par_dir=config["PATH"]["distination_path"] +
+             "/" +
+             config["SETTINGS"]["workbench_dir_name"],
+             ID_dirs=ID_dirs)
