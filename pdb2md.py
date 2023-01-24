3405ed9c3109a1e8b38751004b8dcdac21f2a255
k6atru <k6atru@gmail.com>
2023-01-24 18:50:47 +0900

    adding detail comments

--- pdb2md.py
+++ pdb2md.py
@@ -20,6 +20,14 @@ from pyrosetta.rosetta.protocols import minimization_packing as pack_min
 
 
 def path_to_abspath(path: str) -> str:
+    """Normalize directory names.
+
+    Args:
+        path (str): Path to be normalized
+
+    Returns:
+        str: Normalized path
+    """
     if path:
         if "~" in path:
             path = os.path.expanduser(path)
@@ -30,6 +38,16 @@ def path_to_abspath(path: str) -> str:
 
 
 def make_id_dirs(id_list: list, dist_dir: str, dir_name: str) -> dict:
+    """Create directories in workbench with the name of the PDB or uniprot ID.
+
+    Args:
+        id_list (list): PDB or uniprot ID list
+        dist_dir (str): Directory path to create workbench
+        dir_name (str): Workbench name
+
+    Returns:
+        dict: Dictionary of ID and directory path
+    """
     dist_dir = path_to_abspath(dist_dir)
     workbench_dir_path = f"{dist_dir}/{dir_name}"
     os.makedirs(workbench_dir_path, exist_ok=True)
@@ -43,6 +61,17 @@ def make_id_dirs(id_list: list, dist_dir: str, dir_name: str) -> dict:
 
 def insert_templete_residue(pdb_path: str, templete_pdb_path: str,
                             residue_name: str, output_pdb_name: str) -> str:
+    """Copy a residue with a given name from a template protein based on structural alignment.
+
+    Args:
+        pdb_path (str): Path of the pdb file into which the residues are to be inserted
+        templete_pdb_path (str): Path of the pdb file to be used as a template
+        residue_name (str): Residue name in the template structure to be copied
+        output_pdb_name (str): Name of the pdb file to be inserted residues
+
+    Returns:
+        str: Path of the pdb file to be used as a template
+    """
     pdb_path = path_to_abspath(pdb_path)
     templete_pdb_path = path_to_abspath(templete_pdb_path)
     pymol2_session = pymol2.PyMOL()
@@ -59,6 +88,14 @@ def insert_templete_residue(pdb_path: str, templete_pdb_path: str,
 
 
 def res3to1(res: str) -> str:
+    """Convert amino acids in 3-letter notation to 1-letter notation
+
+    Args:
+        res (str): Amino acids in 3-letter notation
+
+    Returns:
+        str: Amino acids in 1-letter notation
+    """
     if res == "ALA":
         return "A"
     elif res == "ARG":
@@ -104,6 +141,15 @@ def res3to1(res: str) -> str:
 
 
 def remove_disordered_residues(pdb_path: str, output_pdb_name: str) -> str:
+    """Remove disordered area using the metapredict package to reduce computation
+
+    Args:
+        pdb_path (str): Path of the pdb file to be removed disordered area
+        output_pdb_name (str): Pdb file name after removing the disordered area
+
+    Returns:
+        str: Path of pdb file after removing the disordered area
+    """
     pdb_path = path_to_abspath(pdb_path)
     PDBparser = PDB.PDBParser()
     seq = ""
@@ -139,6 +185,17 @@ def remove_disordered_residues(pdb_path: str, output_pdb_name: str) -> str:
 def preparemd_settings_to_list(
     config: configparser.ConfigParser, pdb_path: str, distdir: str, preparemd_path: str
 ) -> list:
+    """Parse the preparemd settings in config.ini
+
+    Args:
+        config (configparser.ConfigParser): ConfigParser object reading config.ini
+        pdb_path (str): Path of the pdb file to preparemd
+        distdir (str): Path of directory to output preparemd results
+        preparemd_path (str): Path to preparemd scripts
+
+    Returns:
+        list: List of formatted preparemd arguments and their values
+    """
     preparemd_config = config.items("PREPAREMD_SETTINGS")
     distdir = path_to_abspath(distdir)
     pdb_path = path_to_abspath(pdb_path)
@@ -164,20 +221,40 @@ def preparemd_settings_to_list(
     return preparemd_cmd
 
 
-def remove_hydrogen(output_pdb_dir, output_pdb_name) -> str:
+def remove_hydrogen(pdb_path: str, output_pdb_dir: str, output_pdb_name: str) -> str:
+    """Removing water from pdb files output from rosetta
+
+    Args:
+        pdb_path (str): Pdb file to remove water (HOH)
+        output_pdb_dir (str): Path of directory where PDB files will be saved after water removal
+        output_pdb_name (str): Name of PDB file after water removal
+
+    Returns:
+        str: Path of PDB file after removing water
+    """
     pymol2_session = pymol2.PyMOL()
     pymol2_session.start()
-    pymol2_session.cmd.load(f"{output_pdb_dir}/{output_pdb_name}", "packed")
+    pymol2_session.cmd.load(pdb_path, "packed")
     pymol2_session.cmd.select("H", "hydro")
     pymol2_session.cmd.save(f"{output_pdb_dir}/{output_pdb_name}", "packed and not H")
     pymol2_session.stop()
-    pdb_path = os.path.join(output_pdb_dir, output_pdb_name)
-    return pdb_path
+    output_pdb_path = os.path.join(output_pdb_dir, output_pdb_name)
+    return output_pdb_path
 
 
 def rosetta_packing_residues(
     pdb_path: str, output_pdb_dir: str, output_pdb_name: str
 ) -> str:
+    """Perform packing (residue side-chain structure optimization) with PyRosetta
+
+    Args:
+        pdb_path (str): Pdb file to be packed
+        output_pdb_dir (str): Path of directory where PDB files will be saved after Rosetta packing
+        output_pdb_name (str): Name of PDB file after Rosetta packing
+
+    Returns:
+        str: Path of PDB file after Rosetta packing
+    """
 
     pdb_path = path_to_abspath(pdb_path)
     output_pdb_dir = path_to_abspath(output_pdb_dir)
@@ -190,15 +267,8 @@ def rosetta_packing_residues(
 
     pose = io.pose_from_pdb(pdb_path)
 
-    # taskfactoryをつくって読み込まないとcore.pack.interaction_graph.interaction_graph_factory: Instantiating DensePDInteractionGraphで止まる
-    # このクラスはpose, rotamer sets, packer task and score functionが設定されないと止まる
     tf = TaskFactory()
-
-    # tf.push_back(operation.InitializeFromCommandline())
-    # 次の行をコメントアウトするとcore.pack.interaction_graph.interaction_graph_factory: Instantiating DensePDInteractionGraphで止まる
     tf.push_back(operation.RestrictToRepacking())
-    # importでtaskfactoryとoperationCOしたら止まった
-    # たぶんデフォルトで作られるインスタンスが悪さしてる
 
     packer = pack_min.PackRotamersMover()
     packer.task_factory(tf)
@@ -209,12 +279,21 @@ def rosetta_packing_residues(
     pose.dump_pdb(f"{output_pdb_dir}/{output_pdb_name}")
 
     pdb_path = remove_hydrogen(
-        output_pdb_dir=output_pdb_dir, output_pdb_name=output_pdb_name
+        pdb_path=pdb_path, output_pdb_dir=output_pdb_dir, output_pdb_name=output_pdb_name
     )
     return pdb_path
 
 
 def make_qscript(workbench_dir: str, id_dirs: dict) -> str:
+    """Create a shell script (qsub.sh) to submit output by preparemd to yayoi at once
+
+    Args:
+        workbench_dir (str): Path of workbench directory
+        id_dirs (dict): Dictionary of ID and directory path
+
+    Returns:
+        str: Path to output shell scripts
+    """
     dir_list_for_sh_script = ""
     for ID, dir in id_dirs.items():
         dir_list_for_sh_script += f"\t{os.path.basename(dir)}\n"
@@ -238,6 +317,15 @@ done"""
 
 
 def make_initscript(workbench_dir: str, id_dirs: dict) -> str:
+    """Create a shell script (init.sh) that outputs a batch of PDB files with initial structure from MD results
+
+    Args:
+        workbench_dir (str): Path of workbench directory
+        id_dirs (dict): Dictionary of ID and directory path
+
+    Returns:
+        str: Path to output shell scripts
+    """
     dir_list_for_sh_script = ""
     for ID, dir in id_dirs.items():
         dir_list_for_sh_script += f"\t{os.path.basename(dir)}\n"
@@ -262,6 +350,15 @@ done"""
 
 
 def download_pdb_files(pdb_id: str, id_dir: str) -> str:
+    """Download PDB files of PDB (in case of 4-letter ID) or uniprot ID
+
+    Args:
+        pdb_id (str): PDB or uniprot ID
+        id_dir (str): Dictionary of ID and directory path
+
+    Returns:
+        str: Path to pdb file downloaded
+    """
     id_dir = path_to_abspath(id_dir)
     if len(pdb_id) == 4:
         print(f"Downloading {pdb_id}...")
@@ -279,7 +376,7 @@ def download_pdb_files(pdb_id: str, id_dir: str) -> str:
         url = f"https://alphafold.ebi.ac.uk/files/AF-{pdb_id}-F1-model_v3.pdb"
         data = requests.get(url).content
         if "not exist" in str(data):
-            print(f"Error: uniplotID {pdb_id} is not found")
+            print(f"Error: uniprotID {pdb_id} is not found")
             return None
         with open(f"{id_dir}/AF-{pdb_id}-F1-model_v3.pdb", "wb") as f:
             f.write(data)
@@ -287,15 +384,29 @@ def download_pdb_files(pdb_id: str, id_dir: str) -> str:
             return os.path.abspath(f.name)
 
 
-def remove_alreadyexist_workbench(workbench_dir: str, flag: bool) -> None:
+def remove_alreadyexist_workbench(workbench_dir: str) -> None:
+    """Delete directory if a workbench with the same name exists
+
+    Args:
+        workbench_dir (str): Path of workbench directory
+    """
     if os.path.isdir(workbench_dir):
         print(f"{workbench_dir} already exists")
-        if flag is True:
-            shutil.rmtree(workbench_dir)
-            print(f"Removed {workbench_dir}.")
+        shutil.rmtree(workbench_dir)
+        print(f"Removed {workbench_dir}.")
 
 
 def convert_complex_to_monomer(id_dir: str, pdb_path: str, output_pdb_name: str) -> str:
+    """Convert (downloaded) pdb file to monomer
+
+    Args:
+        id_dir (str): Dictionary of ID and directory path
+        pdb_path (str): Path of the pdb file to be converted to monomer
+        output_pdb_name (str): Name of th pdb file converted to monomer
+
+    Returns:
+        str: Parh to the pdb file converted to monomer
+    """
     pymol2_session = pymol2.PyMOL()
     pymol2_session.start()
     pymol2_session.cmd.load(pdb_path)
@@ -306,7 +417,16 @@ def convert_complex_to_monomer(id_dir: str, pdb_path: str, output_pdb_name: str)
     return os.path.join(id_dir, output_pdb_name)
 
 
-def get_uniplotid_from_pdbid(pdb_id: str, pretty: str = False):
+def get_uniprotid_from_pdbid(pdb_id: str, pretty: str = False) -> str:
+    """Obtain the corresponding uniprot ID from the PDB ID
+
+    Args:
+        pdb_id (str): PDB ID
+        pretty (str, optional): Whether to output the result in a pretty format. Defaults to False.
+
+    Returns:
+        str: uniprot ID
+    """
     pdb_id = pdb_id.lower()
     full_url = f"https://www.ebi.ac.uk/pdbe/api/mappings/uniprot/{pdb_id}?pretty={str(pretty).lower()}"
     json_results = requests.get(full_url).json()
@@ -318,63 +438,99 @@ def get_uniplotid_from_pdbid(pdb_id: str, pretty: str = False):
     return uniprot_id4
 
 
-def download_fasta(pdb_id: str, modeller_dir: str) -> str:
-    modeller_dir = path_to_abspath(modeller_dir)
-    uniplot_id = get_uniplotid_from_pdbid(pdb_id)
-    print(f"Downloading FASTA file of {pdb_id} = {uniplot_id}...")
-    url = f"https://rest.uniprot.org/uniprotkb/{uniplot_id}.fasta"
+def download_fasta(pdb_id: str, output_dir: str) -> str:
+    """Download fasta file from specified PDB or Uniprot ID
+
+    Args:
+        pdb_id (str): PDB or Uniprot ID
+        output_dir (str): Path of the directory to save the fasta file obtained
+
+    Returns:
+        str: Path of the fasta file downloaded
+    """
+    output_dir = path_to_abspath(output_dir)
+    uniprot_id = get_uniprotid_from_pdbid(pdb_id)
+    print(f"Downloading FASTA file of {pdb_id} = {uniprot_id}...")
+    url = f"https://rest.uniprot.org/uniprotkb/{uniprot_id}.fasta"
+
+    fasta_path = f"{output_dir}/{pdb_id}.fasta"
+    fasta_path = path_to_abspath(fasta_path)
 
     data = requests.get(url).content
+
     if "not exist" in str(data):
-        print(f"Error: pdbID {pdb_id} is not found")
+        print(f"Error occurred while downloading fasta file: pdbID {pdb_id} is not found")
         pass
     else:
-        with open(f"{modeller_dir}/{pdb_id}.fasta", "wb") as f:
+        with open(fasta_path, "wb") as f:
             f.write(data)
             f.close()
-    fasta_path = f"{modeller_dir}/{pdb_id}.fasta"
-    fasta_path = path_to_abspath(fasta_path)
     return fasta_path
 
 
 def change_align_code_in_fasta(
-    fasta_path: str, align_codes: str, alignment_format: str
+    fasta_path: str, align_codes: str, alignment_format: str="fasta"
 ) -> str:
+    """Parse fasta file and change align code
+
+    Args:
+        fasta_path (str): Path of fasta file
+        align_codes (str): Align code
+        alignment_format (str): Alignment format of fasta file. Defaults to "fasta"
+
+    Returns:
+        str: Align code
+    """
     fasta_path = path_to_abspath(fasta_path)
-    record = SeqIO.parse(fasta_path, alignment_format)
+    for record in SeqIO.parse(fasta_path,
+                              alignment_format):
+        id = record.id
+        desc = record.description
+        seq = record.seq
     record.id = align_codes
     SeqIO.write(record, fasta_path, alignment_format)
     return align_codes
 
 
 def modelling_missing_res(
-    pdb_id: str, id_dir: str, modeller_dir: str, fasta_path: str, pdb_path: str
+    pdb_id: str, id_dir: str, output_dir: str, fasta_path: str, pdb_path: str
 ) -> str:
+    """Constructing Missing Regions by modeller. Output the highest scoring results as pdb file
 
-    modeller_dir = path_to_abspath(modeller_dir)
+    Args:
+        pdb_id (str): PDB or Uniplot ID
+        id_dir (str): Path to ID directory
+        output_dir (str): Path of the directory where files related to the work done by the modeller are stored
+        fasta_path (str): Path of fasta file
+        pdb_path (str): Path of pdb file
+
+    Returns:
+        str: Path of the output pdb file of modeller
+    """
+
+    output_dir = path_to_abspath(output_dir)
     fasta_path = path_to_abspath(fasta_path)
     pdb_path = path_to_abspath(pdb_path)
 
     code_fill = change_align_code_in_fasta(
-        fasta_path=fasta_path, align_codes=f"{pdb_id}_fill", alignment_format="fasta"
-    )
+        fasta_path=fasta_path, align_codes=f"{pdb_id}_fill")
 
     modeller.log.none()
     env = modeller.Environ()
     aln = modeller.Alignment(env)
-    env.io.atom_files_directory = [modeller_dir]
+    env.io.atom_files_directory = [output_dir]
     aln.append(file=fasta_path, align_codes=code_fill, alignment_format="fasta")
     mdl = modeller.Model(env, file=pdb_path)
     aln.append_model(mdl, align_codes=pdb_id)
     aln.salign(rms_cutoff=3.5, normalize_pp_scores=False)
-    aln.write(file=f"{modeller_dir}/{pdb_id}.ali", alignment_format="pir")
+    aln.write(file=f"{output_dir}/{pdb_id}.ali", alignment_format="pir")
 
-    os.chdir(modeller_dir)
+    os.chdir(output_dir)
     env = modeller.Environ()
     env.io.atom_files_directory = [id_dir]
     env.io.hetatm = True
     env.io.water = True
-    aln_file = f"{modeller_dir}/{pdb_id}.ali"
+    aln_file = f"{output_dir}/{pdb_id}.ali"
     a = automodel.AutoModel(env, alnfile=aln_file, knowns=pdb_id, sequence=code_fill)
     a.md_level = automodel.refine.fast
     a.starting_model = 1
@@ -388,7 +544,7 @@ def modelling_missing_res(
     min_molpdf_index = molpdf_list.index(min_molpdf)
     min_model = a.outputs[min_molpdf_index]["name"]
 
-    pdb_path_modelled = f"{modeller_dir}/{min_model}"
+    pdb_path_modelled = f"{output_dir}/{min_model}"
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     return pdb_path_modelled
@@ -420,13 +576,14 @@ workbench_dir = os.path.join(
 )
 workbench_dir = path_to_abspath(workbench_dir)
 
-remove_alreadyexist_workbench(workbench_dir=workbench_dir, flag=flg_new_workbench)
+if flg_new_workbench == True:
+    remove_alreadyexist_workbench(workbench_dir=workbench_dir)
 
 id_list: list = [key.upper() for key in config["ID"]]
 id_dirs: dict = make_id_dirs(
     id_list=id_list,
     dist_dir=config["PATH"]["distination_path"],
-    dir_name=config["SETTINGS"]["workbench_dir_name"],
+    dir_name=config["SETTINGS"]["workbench_dir_name"]
 )
 id_pdb_paths: dict = {}
 
@@ -455,7 +612,7 @@ if flg_comp_to_mono is True:
             id_pdb_paths[ID] = convert_complex_to_monomer(
                 id_dir=id_dirs[ID],
                 pdb_path=pdb_path,
-                output_pdb_name=f"{ID}_monomer.pdb",
+                output_pdb_name=f"{ID}_monomer.pdb"
             )
 
 if flg_model_missing is True:
@@ -463,14 +620,14 @@ if flg_model_missing is True:
         if len(ID) == 4:
             modeller_dir = os.path.join(id_dirs[ID], "modeller")
             os.mkdir(modeller_dir)
-            fasta_path = download_fasta(pdb_id=ID, modeller_dir=modeller_dir)
+            fasta_path = download_fasta(pdb_id=ID, output_dir=modeller_dir)
             print(f"Modelling missing residue of {ID}...")
             id_pdb_paths[ID] = modelling_missing_res(
                 pdb_id=ID,
                 id_dir=id_dirs[ID],
-                modeller_dir=modeller_dir,
+                output_dir=modeller_dir,
                 fasta_path=fasta_path,
-                pdb_path=pdb_path,
+                pdb_path=pdb_path
             )
 
 if flg_rm_disorder is True:
